f7ecf4ad10237cddcf811ca45d9049dfde11d247
Tobias Kündig <8600029+tobias-kuendig@users.noreply.github.com>
2023-01-03 14:46:45 +0100

    Login View

--- twitter/src/views/LoginView.vue
+++ twitter/src/views/LoginView.vue
@@ -1,4 +1,30 @@
 <template>
-  Login
+  <div class="login">
+    <section class="login-wrapper">
+      <h2>Login</h2>
+
+      <form action="#" class="login-form">
+        <div class="form-group">
+          <label class="form-label" for="username">Benutzername</label>
+          <input class="form-input" type="text" id="username" />
+          <div class="form-error">
+            Unbekannter Benutzername
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="form-label" for="password">Passwort</label>
+          <input class="form-input" type="password" id="password" />
+          <div class="form-error">
+            Falsches Passwort
+          </div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn--primary btn--block">
+            Login
+          </button>
+        </div>
+      </form>
+    </section>
+  </div>
 </template>
 
